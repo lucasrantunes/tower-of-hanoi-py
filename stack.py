122f01facc42614f32c44d6066f4b7f0fa36c5ae
lucasrantunes <lucasribeiroantunes2013@gmail.com>
2022-01-05 23:58:56 -0300

    Stack essential functions

--- stack.py
+++ stack.py
@@ -1,10 +1,29 @@
-from disk import Disk
-
-
 from disk import Disk 
 class Stack:
-    def __init__(self, num_disks: int = 0):
-        self.num_disks = num_disks
-        self.disks = []
-        for width in range(1,num_disks+1):
-            self.disks.append(Disk(width))
+    def __init__(self, size: int = 0):
+        self.size = size
+
+        # insert disks in the stack
+        if self.size > 0 :
+            self.disks = []
+            for width in range(1,size+1):
+                self.disks.append(Disk(width))
+
+
+    def size(self):
+        return self.size
+
+
+    def peek_top(self):
+        return self.disks[self.size - 1]
+    
+
+    def remove_top(self):
+        disk = self.disks[self.size - 1]
+        self.disks.pop()
+        self.size = self.size - 1
+        return disk
+
+    
+    def push_back(self, disk):
+        self.disks.append(disk)
