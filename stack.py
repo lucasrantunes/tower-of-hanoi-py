6c2d0da059f2ff2c029fa48ef0a903a4f27c38ff
lucasrantunes <lucasribeiroantunes2013@gmail.com>
2022-01-06 19:13:40 -0300

    changes in get size method

--- stack.py
+++ stack.py
@@ -1,33 +1,30 @@
 from disk import Disk 
 class Stack:
     def __init__(self, size: int = 0):
-        self.size = size
         self.disks = []
 
         # insert disks in the stack
-        if self.size > 0 :
+        if size > 0 :
             for width in range(size, 0, -1):
                 self.disks.append(Disk(width))
 
 
     def get_size(self):
-        return self.size
+        return len(self.disks)
 
 
     def peek_top(self):
-        return self.disks[self.size - 1]
+        return self.disks[self.get_size() - 1]
     
 
     def remove_top(self):
-        disk = self.disks[self.size - 1]
+        disk = self.disks[self.get_size() - 1]
         self.disks.pop()
-        self.size = self.size - 1
         return disk
 
     
     def push_back(self, disk):
         self.disks.append(disk)
-        self.size += 1
 
 
     def stack_status(self):
